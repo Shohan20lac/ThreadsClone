e53ec09c4a7c59b3bcd24e47b13da47d2818c378
shohan20lac <r.shohan12@gmail.com>
2023-10-01 20:07:26 +0600

    1:10:22
    - Created placeholder layouts for Homepage and all navbars
    - Implemented Sign In and Sign Out using Clerk
    - Started defining AccountProfile component
    - TODO: use shadcn components to implement AccountProfile

---
+++ components/shared/forms/AccountProfile.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import Image from "next/image";
+
+
+interface Props {
+    user: {
+      id:       string;
+      objectId: string;
+      username: string;
+      name:     string;
+      bio:      string;
+      image:    string;
+    };
+    btnTitle: string;
+  }
+
+const AccountProfile = ({user, btnTitle} : Props) => {
+    return (
+        <div>
+            Account Profile
+        </div>
+    )
+}
+
+export default AccountProfile;
